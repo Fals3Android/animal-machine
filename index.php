61bd7fe8ae9040aa579f4223129642e7d9917cae
Fals3Android <davilasturias89@gmail.com>
2015-09-02 19:52:13 -0700

    Core program files v-1.1

---
+++ index.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html lang="utf-8">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/chosen.css">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/skeleton.css">
+    <link rel="stylesheet" href="css/prism.css">
+    <title>Animal Machine</title>
+</head>
+
+<body style="height:100%">
+    <div class="container six columns offset-by-three columns" style="background-color:#A52A2A">
+    <h1 >The Animal Machine</h1>
+    
+    <div class="twelve columns">
+    <h3>What year were you born?</h3>
+     </div>
+       
+    <div class="twelve columns" style="text-align:center">
+<form method="post" action="array.php">  
+<div class="chosen-container chosen-container-single">
+    <select  class="chzn-select" name="Year" tabindex="-1" style="width:250px" data-placeholder="Choose a Year...">
+             <option  value="OX">1901</option>
+             <option  value="TIGER">1902</option>
+             <option  value="HARE">1903</option>
+             <option  value="DRAGON">1904</option>
+             <option  value="SNAKE">1905</option>
+             <option  value="HORSE">1906</option>
+             <option  value="SHEEP">1907</option>
+             <option  value="MONKEY">1908</option>
+             <option  value="ROOSTER">1909</option>
+             <option  value="DOG">1910</option>
+             <option  value="PIG">1911</option>
+             <option  value="RAT">1912</option>
+             <option  value="OX">1913</option>
+             <option  value="TIGER">1914</option>
+             <option  value="HARE">1915</option>
+             <option  value="DRAGON">1916</option>
+             <option  value="SNAKE">1917</option>
+             <option  value="HORSE">1918</option>
+             <option  value="SHEEP">1919</option>
+             <option  value="MONKEY">1920</option>
+             <option  value="ROOSTER">1921</option>
+             <option  value="DOG">1922</option>
+             <option  value="PIG">1923</option>
+             <option  value="RAT">1924</option>
+             <option  value="OX">1925</option>
+             <option  value="TIGER">1926</option>
+             <option  value="HARE">1927</option>
+             <option  value="DRAGON">1928</option>
+             <option  value="SNAKE">1929</option>
+             <option  value="HORSE">1930</option>
+             <option  value="SHEEP">1931</option>
+             <option  value="MONKEY">1932</option>
+             <option  value="ROOSTER">1933</option>
+             <option  value="DOG">1934</option>
+             <option  value="PIG">1935</option>
+             <option  value="RAT">1936</option>
+             <option  value="OX">1937</option>
+             <option  value="TIGER">1938</option>
+             <option  value="HARE">1939</option>
+             <option  value="DRAGON">1940</option>
+             <option  value="SNAKE">1941</option>
+             <option  value="HORSE">1942</option>
+             <option  value="SHEEP">1943</option>
+             <option  value="MONKEY">1944</option>
+             <option  value="ROOSTER">1945</option>
+             <option  value="DOG">1946</option>
+             <option  value="PIG">1947</option>
+             <option  value="RAT">1948</option>
+             <option  value="OX">1949</option>
+             <option  value="TIGER">1950</option>
+             <option  value="HARE">1951</option>
+             <option  value="DRAGON">1952</option>
+             <option  value="SNAKE">1953</option>
+             <option  value="HORSE">1954</option>
+             <option  value="SHEEP">1955</option>
+             <option  value="MONKEY">1956</option>
+             <option  value="ROOSTER">1957</option>
+             <option  value="DOG">1958</option>
+             <option  value="PIG">1959</option>
+             <option  value="RAT">1960</option>
+             <option  value="OX">1961</option>
+             <option  value="TIGER">1962</option>
+             <option  value="HARE">1963</option>
+             <option  value="DRAGON">1964</option>
+             <option  value="SNAKE">1965</option>
+             <option  value="HORSE">1966</option>
+             <option  value="SHEEP">1967</option>
+             <option  value="MONKEY">1968</option>
+             <option  value="ROOSTER">1969</option>
+             <option  value="DOG">1970</option>
+             <option  value="PIG">1971</option>
+             <option  value="RAT">1972</option>
+             <option  value="OX">1973</option>
+             <option  value="TIGER">1974</option>
+             <option  value="HARE">1975</option>
+             <option  value="DRAGON">1976</option>
+             <option  value="SNAKE">1977</option>
+             <option  value="HORSE">1978</option>
+             <option  value="SHEEP">1979</option>
+             <option  value="MONKEY">1980</option>
+             <option  value="ROOSTER">1981</option>
+             <option  value="DOG">1982</option>
+             <option  value="PIG">1983</option>
+             <option  value="RAT">1984</option>
+             <option  value="OX">1985</option>
+             <option  value="TIGER">1986</option>
+             <option  value="HARE">1987</option>
+             <option  value="DRAGON">1988</option>
+             <option  value="SNAKE">1989</option>
+             <option  value="HORSE">1990</option>
+             <option  value="SHEEP">1991</option>
+             <option  value="MONKEY">1992</option>
+             <option  value="ROOSTER">1993</option>
+             <option  value="DOG">1994</option>
+             <option  value="PIG">1995</option>
+             <option  value="RAT">1996</option>
+             <option  value="OX">1997</option>
+             <option  value="TIGER">1998</option>
+             <option  value="HARE">1999</option>
+             <option  value="DRAGON">2000</option>
+             <option  value="SNAKE">2001</option>
+             <option  value="HORSE">2002</option>
+             <option  value="SHEEP">2003</option>
+             <option  value="MONKEY">2004</option>
+             <option  value="ROOSTER">2005</option>
+             <option  value="DOG">2006</option>
+             <option  value="PIG">2007</option>
+             <option  value="RAT">2008</option>
+             <option  value="OX">2009</option>
+             <option  value="TIGER">2010</option>
+             <option  value="HARE">2011</option>
+             <option  value="DRAGON">2012</option>
+             <option  value="SNAKE">2013</option>
+             <option  value="HORSE">2014</option>
+             <option  value="SHEEP">2015</option>
+             <option  value="MONKEY">2016</option>
+             <option  value="ROOSTER">2017</option>
+             <option  value="DOG">2018</option>
+             <option  value="PIG">2019</option>
+             <option  value="RAT">2020</option>
+             <option  value="OX">2021</option>
+             <option  value="TIGER">2022</option>
+             <option  value="HARE">2023</option>
+             <option  value="DRAGON">2024</option>
+             <option  value="SNAKE">2025</option>
+                
+    </select>
+    </div>
+    <input class="button input" style="width:250px" type="submit" value="Ok, I'm Ready!"/>
+    
+</form>
+    </div> 
+</div>
+    <script src="http://code.jquery.com/jquery-1.8.3.js"></script>
+    <script src="http://code.jquery.com/ui/1.9.2/jquery-ui.js"></script>
+    <script src="js/chosen.jquery.min.js" type="text/javascript"></script>
+    <script src="js/prism.js" type="text/javascript"></script>
+    <script>
+        $(function(){
+    $(".chzn-select").chosen();
+});
+    </script>
+    
+</body>
+</html>
