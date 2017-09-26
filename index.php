92bcbcc77348fecf7649707e51eb569306c3a9b1
Fals3Andro1d <davilasturias89@gmail.com>
2017-09-25 20:33:35 -0700

    Animal Machine
    Set up src folder with variable file and Aztro test class to import
    into the form and the server response, respectively.

--- index.php
+++ index.php
@@ -1,11 +1,5 @@
-<?php
-    //TODO: Move these variables to their own file
-    $animals = ['OX', 'TIGER', 'HARE', 'DRAGON', 'SNAKE', 'HORSE', 'SHEEP', 'MONKEY', 'ROOSTER', 'DOG', 'PIG', 'RAT'];
-    $yrsMin = 1901;
-    $yrsMax = 2025;
-    $countAnimals = 0;
-?>
-
+<?php include 'src/variables.php'; ?>
+<!DOCTYPE html>
 <html>
     <head>
         <meta charset="UTF-8">
@@ -16,25 +10,22 @@
         <link rel="stylesheet" href="css/prism.css">
         <title>Animal Machine</title>
     </head>
-
 <body style="height:100%">
     <div class="container six columns offset-by-three columns" style="background-color:#A52A2A">
     <h1 >The Animal Machine</h1>
-    
     <div class="twelve columns">
-    <h3>What year were you born?</h3>
-     </div>
-       
+        <h3>What year were you born?</h3>
+    </div>   
     <div class="twelve columns" style="text-align:center">
-    <form method="post" action="array.php">  
-    <div class="chosen-container chosen-container-single">
-        <select  class="chzn-select" name="Year" tabindex="-1" style="width:250px" data-placeholder="Choose a Year...">
-            <?php for($i = $yrsMin; $i <= $yrsMax; $i++ ) { ?>
-                <option  value=<?php echo $animals[$countAnimals]; ?> > <?php echo $i; ?> </option>
-                <?php $countAnimals++; if($countAnimals >= sizeof($animals) ) $countAnimals = 0; ?>
-            <?php } ?>  
-        </select>
-    </div>
+    <form method="post" action="animal.php">  
+        <div class="chosen-container chosen-container-single">
+            <select  class="chzn-select" name="Year" tabindex="-1" style="width:250px" data-placeholder="Choose a Year...">
+                <?php for($i = $yrsMin; $i <= $yrsMax; $i++ ) { ?>
+                    <option  value=<?php echo $animals[$countAnimals]; ?> > <?php echo $i; ?> </option>
+                    <?php $countAnimals++; if($countAnimals >= sizeof($animals) ) $countAnimals = 0; ?>
+                <?php } ?>  
+            </select>
+        </div>
         <input class="button input" style="width:250px" type="submit" value="Ok, I'm Ready!"/>
     </form>
     </div> 
---
+++ src/variables.php
@@ -0,0 +1,6 @@
+<?php
+
+	$animals = ['OX', 'TIGER', 'HARE', 'DRAGON', 'SNAKE', 'HORSE', 'SHEEP', 'MONKEY', 'ROOSTER', 'DOG', 'PIG', 'RAT'];
+    $yrsMin = 1901;
+    $yrsMax = 2025;
+    $countAnimals = 0;
